a145add935a06b71732f60ce577b7bb8e40a03a1
Vickie Gicheru <vickiegicheru6373@gmail.com>
2025-06-04 11:47:50 +0100

    Save local changes before pull

---
+++ future_age_calculator.py
@@ -0,0 +1,4 @@
+New-Item - Name "future_age_calculator.py" - ItemType "File"
+current_age = int(input("How old are you?"))
+future_age = current_age + 27
+print(f"In 2050, you will be {future_age} years old.")
--- rectangle_area.py
+++ rectangle_area.py
@@ -1,4 +1,4 @@
 length = 10
 width = 5
-area_of_rectangle = length * width
-print(area_of_rectangle)
+area = length * width
+print(f"The area of the rectangle is: {area}")
